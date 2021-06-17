a1d09ec585a2fa2ce62eba7f288f6cd0e9f50b3c
Oreoluwa <oreeboy@gmail.com>
2021-06-17 13:12:10 +0100

    feat: setup default layout

---
+++ components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="section__grey">
+    <footer class="footer">
+      <nav class="footer-nav">
+        <nuxt-link to="/privacy" class="footer-nav__link">
+          Privacy
+        </nuxt-link>
+        <nuxt-link to="/terms" class="footer-nav__link">
+          Terms
+        </nuxt-link>
+      </nav>
+      <p class="footer-copyright">
+        © 2021, Biya. All Rights Reserved.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section__grey {
+  background: #F5F5F5;
+}
+.footer {
+  max-width: 130rem;
+  margin: 0 auto;
+  padding: 3.2rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--primary-color);
+  font-size: 1.6rem;
+  &-nav {
+    &__link {
+      color: inherit;
+      text-decoration: none;
+      &:hover {
+        color: var(--pink);
+        &::after {
+          color:  var(--primary-color);
+        }
+      }
+      &:first-of-type {
+        &::after {
+          content: '|'
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 600px) {
+    text-align: center;
+    flex-direction: column;
+    &-nav {
+      margin-bottom: 1rem;
+    }
+  }
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="nav">
+    <nuxt-link to="/">
+      <img src="@/assets/logo.png" alt="Biya - Easy payments via chat" class="nav__image">
+    </nuxt-link>
+    <nuxt-link to="/" class="nav__link">
+      FAQs
+    </nuxt-link>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.nav {
+  max-width: 130rem;
+  margin: 0 auto;
+  padding: 6rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &__image {
+    height: 4rem;
+    width: auto;
+  }
+  &__link {
+    font-size: 1.6rem;
+    line-height: 1.5;
+    color: #5A6972;
+    text-decoration: none;
+    &:hover {
+      color: var(--pink);
+    }
+  }
+}
+</style>
